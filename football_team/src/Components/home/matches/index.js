4941a09f9c6d878e960255910906bf97bcc59681
Doniyor4120 <36728197+Doniyor4120@users.noreply.github.com>
2019-07-14 19:22:40 +0500

    new commit

---
+++ football_team/src/Components/home/matches/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MatchesHome = () => {
+    return (
+        <div className="home_matches_wrapper">
+            <div className="container">
+                TAG
+
+                BLOCK
+
+                TAG
+
+            </div>
+        </div>
+    );
+};
+
+export default MatchesHome;
